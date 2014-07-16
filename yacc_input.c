52f70f777b32d4b4fd7d7b299dd346c02de54f10
Your Name <you@example.com>
2014-07-16 22:17:10 +0100

    Compilation 0.0

--- yacc_input.c
+++ yacc_input.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include "structures.h"
 
-void processAssignment(char * subject, struct TVerb * , char* object);
+void processAssignment(char * subject, struct TVerb * tverb , char* object){
+}
 
 struct Message {
  char * input, * output;
@@ -28,49 +29,30 @@ struct Message {
 %type<sValue> subject
 %type<sValue> object
 
-%token BRACKET_OPEN
-%token BRACKET_CLOSE
-%token BRACES_OPEN
-%token BRACES_CLOSE
-%token SEMICOLON
-%token COMMA
-%token STOP
-%token BLANK
-%token VERB_IS_PRES
-%token VERB_IS_PAST
-%token VERB_DO_SINGLE_PAST
-%token VERB_DO_SINGLE_PRES
-%token WHERE
-%type<pEntity> statement
-%type<pVerb> verb
-%type<pVerb> s_verb
-%type<pVerb> s_do
-%type<pVerb> s_etre
+%token AND
+%token OR
+%token NOT
+%token ADD
+%token SUB
+%token MULT
+%token DIV
+%token EQUALS
+
+%token RES
+%token LT
+%token GT
+
+
+// %type<pVerb> verb
+
+// SAMPLE:
+// verb: blank s_verb { $$ = $2;} | s_verb blank { $$ = $1; } | s_verb { $$ = $1;} | blank s_verb blank { $$ = $2;}
 
 %%
 
-base :
-base STOP base {
-              }
-| statement { printf("statement");
-            }  | base STOP { printf("statement STOP"); }
-;
-
-blank : BLANK | blank BLANK
-where   : blank WHERE | WHERE blank | blank WHERE blank
-
 subject: NAME { $$ = $1; }
 object : NAME { $$ = $1; }
-s_etre : VERB_IS_PRES { $$ = createVerb("is",PRESENT); } | VERB_IS_PAST { $$ = createVerb("is",PAST); }
-s_do : VERB_DO_SINGLE_PRES { $$ = createVerb("do",PRESENT); } | VERB_DO_SINGLE_PAST { $$ = createVerb("do",PAST); }
-s_verb : s_do { $$ = $1; } | s_etre { $$= $1;}
 
-verb: blank s_verb { $$ = $2;} | s_verb blank { $$ = $1; } | s_verb { $$ = $1;} | blank s_verb blank { $$ = $2;}
-
-active_voice : subject verb object { processAssignment($1,$2,$3); }
-
-statement: active_voice { printf( "<-- Active Voice -->") ; } |
-    where verb subject { printf("<-- Interrogative -->"); }
 
 %%
 
