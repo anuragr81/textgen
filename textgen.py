aec2be9911001b642e54c504cae5c2913d1bf7b7
anurag <anandmukh@yahoo.com@git config --global user.name anuraggit config --global user.email anandmukh@yahoo.com@>
2018-03-12 14:38:04 +0000

    Adding property facility

--- textgen.py
+++ textgen.py
@@ -1,9 +1,10 @@
 import sys
-from pyparsing import Word,alphas
+from pyparsing import Word,alphas,OneOrMore
 
 kt = {}
 
-def updateKT(tokens):
+def updateKT(s,location,tokens):
+        print "location=",location, "s=",s, "tokens=",tokens
 	kt[tokens[0]]=tokens[2:]
 
 """
@@ -21,21 +22,26 @@ def document_from_template(templateTree):
 
 
 # define grammar
-
-greet = Word( alphas ) + "=" + Word( alphas )
-
-greet.setParseAction(updateKT)
-
-# input string
-hello = "love=false"
-
-# parse input string
-print hello, "->", greet.parseString( hello )
-print "KT=",kt
+variable = Word(alphas) | Word(alphas)+ "::"+ Word(alphas)
+freeformText = "freeform"+"("+ OneOrMore(Word(alphas))+")"
+rvalue = freeformText | Word(alphas)
+grammar= variable + "::" + Word(alphas) + "=" + rvalue | variable + "=" + rvalue
+grammar.setParseAction(updateKT)
 
 
-
-
+if __name__ == "__main__":
+    # input string
+    inputString = sys.argv[1]
+    
+    # parse input string
+    print inputString, "->", grammar.parseString( inputString )
+    print "KT=",kt
+else:
+    print "__name__=",__name__
+    
+    
+    
+    
 ### Grammar for knowledge tree - how would a knowledge bank look like
 # Every command is that of a context - there are predefined contexts for now
 # "#start-context Company" (Company predefined contexts)
